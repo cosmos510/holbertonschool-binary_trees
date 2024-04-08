870e2fd4340ef6af4830590fccb06ca87d8c6c93
NadegeL <nadege.luthier@gmail.com>
2024-04-08 21:57:33 +0200

    betty changes

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -6,8 +6,8 @@
 */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-    if (node != NULL && node->right != NULL && node->left != NULL)
-        return (0);
-    else
-        return (1);
+	if (node != NULL && node->right != NULL && node->left != NULL)
+		return (0);
+	else
+		return (1);
 }
