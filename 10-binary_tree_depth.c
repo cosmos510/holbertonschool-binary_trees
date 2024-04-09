ad0cbf884ef50a99138c2a776da105b2c1fcdcdf
cosmos510 <maximemartin510@gmail.com>
2024-04-09 09:51:39 +0000

    add 10-binary deepth

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_depth - function that measures the deepth of a binary tree
+ * @tree: a pointer to the node root node of the tree to traverse
+ * Return: the deepth of the tree
+*/
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	int deepth = 0;
+
+	if (tree == NULL || tree->parent == NULL)
+		return (0);
+	else
+		deepth = binary_tree_depth(tree->parent);
+
+	return (deepth+ 1);
+}
