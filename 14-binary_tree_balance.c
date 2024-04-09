bd697a90b3977299e031f4cc5e5dbae975df5396
cosmos510 <maximemartin510@gmail.com>
2024-04-09 14:13:49 +0000

    add binary tree balance

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -6,13 +6,9 @@
 */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	int height_right = 0;
-	int height_left = 0;
+	size_t height_left = 0, height_right = 0;
 
 	if (tree == NULL)
-		return (0);
-
-	if (tree->left == NULL && tree->right == NULL)
 		return (0);
 
 	else
@@ -25,7 +21,13 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		return (height_left + 1);
 	return (height_right + 1);
 }
+/**
+ * binary_tree_balance - function that measures the balance factor of
+ * a binary tree
+ * @tree: a pointer to the node root node of the tree to traverse
+ * Return: the balance of the tree
+*/
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	return(binary_tree_height(tree->left) - binary_tree_height(tree->right));
+	return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
 }
