0848d6b6676e657467bf25937a731fea12d1340f
cosmos510 <maximemartin510@gmail.com>
2024-04-09 14:54:48 +0000

    fixing betty doc

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -1,6 +1,6 @@
 #include "binary_trees.h"
 /**
- * binary_tree_siblingt - function that that finds the sibling of a node
+ * binary_tree_sibling - function that that finds the sibling of a node
  * @node: is a pointer to the node to find the sibling
  * Return: the sibling node
 */
