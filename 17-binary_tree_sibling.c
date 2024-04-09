23877c9269efb14ed5485dbfed823cf9f92590cc
cosmos510 <maximemartin510@gmail.com>
2024-04-09 14:47:23 +0000

    changing condition in if

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -9,7 +9,7 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 	if (node == NULL || node->parent == NULL)
 		return (NULL);
 
-	if (node->parent->left != NULL && node->parent->right == node)
+	if (node->parent->right == node)
 		return (node->parent->left);
 
 	return (node->parent->right);
