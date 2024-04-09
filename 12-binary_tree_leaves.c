f496e0a931f327b355825723701ff594a43a84fa
cosmos510 <maximemartin510@gmail.com>
2024-04-09 12:12:04 +0000

    add 12 binary tree leaves

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -6,15 +6,16 @@
 */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-	int count_l = 0;
 
 	if (tree == NULL)
 		return (0);
 
-	if (tree->left != NULL && tree->right != NULL)
+	if (tree->left == NULL && tree->right == NULL)
 	{
-		count_l = binary_tree_leaves(tree->left);
-		count_l = binary_tree_leaves(tree->right);
+		return(1);
 	}
-	return(count_l + 1);
+	else
+		return(binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
+
+
 }
