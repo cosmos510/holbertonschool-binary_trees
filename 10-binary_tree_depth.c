d4f149fb6b7f07630fc4c31df10b64f885738423
cosmos510 <maximemartin510@gmail.com>
2024-04-09 09:56:23 +0000

    add else condition

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -11,7 +11,8 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 	if (tree == NULL || tree->parent == NULL)
 		return (0);
 
-	deepth = binary_tree_depth(tree->parent);
+	else
+		deepth = binary_tree_depth(tree->parent);
 
 	return (deepth + 1);
 }
