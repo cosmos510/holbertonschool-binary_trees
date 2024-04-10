d0944a66d9f7508e3b161339486247006a406abc
cosmos510 <maximemartin510@gmail.com>
2024-04-10 07:08:34 +0000

    changing condition

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -33,19 +33,16 @@ size_t binary_tree_height(const binary_tree_t *tree)
 */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	int depth_left = 0, depth_right = 0;
+	int depth_left, depth_right;
 
 	if (tree == NULL)
 		return (0);
 
 	depth_left = binary_tree_height(tree->left);
 	depth_right = binary_tree_height(tree->right);
-
+	printf("%d\n", depth_left);
+	printf("%d\n", depth_right);
 	if (depth_left == depth_right)
-	{
-		if (binary_tree_is_perfect(tree->left) &&
-		binary_tree_is_perfect(tree->right))
 			return (1);
-	}
 return (0);
 }
